587ed0fdd0c015fc6c48af4fbbb56cc124624f9d
genkimatsubara <77258065+genkimatsubara@users.noreply.github.com>
2021-08-17 00:17:14 +0900

    Update if-string-numeric.rb

--- Ruby/if-string-numeric.rb
+++ Ruby/if-string-numeric.rb
@@ -1,1 +1,18 @@
+# 数値の比較
+score = 10
 
+if score > 1
+  puts "1より大きい"
+else
+  puts "1より小さい"
+end
+
+
+# 文字列の比較
+greeting = "hello"
+
+if greeting == "hello"
+  puts "等しい"
+else
+  puts "等しくない"
+end
